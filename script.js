68ef3c99195422a01aebd81b89f22fa438a85b4d
David L <lin.david32@gmail.com>
2022-06-08 19:31:29 -0700

    unit switch logic and further layout changes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 class Weather {
-    constructor(city, country, feels_like, temp, high, low) {
+    constructor(city, country, type, icon, desc, feels_like, temp, high, low) {
         this.city = city;
         this.country = country;
+        this.type = type;
+        this.icon = icon;
+        this.desc = desc;
         this.feels_like = feels_like;
         this.temp = temp;
         this.high = high;
@@ -9,19 +12,40 @@ class Weather {
     }
 }
 
-function generateMap(lat, lon) {
-    var map = L.map("map").setView([lat, lon], 12);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution: "© OpenStreetMap",
-    }).addTo(map);
-    L.marker([lat, lon]).addTo(map);
-}
+const interactiveMap = (() => {
+    let map;
+    let layer;
+    const setMap = (lat, lon) => {
+        if (map) {
+            map.setView([lat, lon], 12);
+            layer.clearLayers();
+        } else {
+            map = L.map("map").setView([lat, lon], 12);
+            layer = L.layerGroup().addTo(map);
+        }
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: "© OpenStreetMap",
+        }).addTo(map);
+        L.marker([lat, lon]).addTo(layer);
+    };
+    return { setMap };
+})();
 
 const forecast = (() => {
     let weather;
     let fahrenheit = true;
-    const setWeather = (city, country, feels_like, temp, high, low) => {
+    const setWeather = (
+        city,
+        country,
+        type,
+        icon,
+        desc,
+        feels_like,
+        temp,
+        high,
+        low
+    ) => {
         if (fahrenheit) {
             // array funcs?
             feels_like = (feels_like - 273.15) * (9 / 5) + 32;
@@ -37,6 +61,9 @@ const forecast = (() => {
         weather = new Weather(
             city,
             country,
+            type,
+            icon,
+            desc,
             feels_like.toFixed(1),
             temp.toFixed(1),
             high.toFixed(1),
@@ -47,14 +74,16 @@ const forecast = (() => {
         console.log(weather);
         return weather;
     };
-    const fetchWeather = async () => {
+    const fetchWeather = async (city) => {
+        const loading = document.querySelector(".loading");
+        loading.classList.remove("hidden");
         const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&appid=21f4d3accb72400f5b07454c7190562f`,
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=21f4d3accb72400f5b07454c7190562f`,
             { mode: "cors" }
         );
         try {
             const data = await response.json();
-            if (data.cod == "404") {
+            if (data.cod != "200") {
                 throw new Error(data.message);
             }
             console.log(data);
@@ -63,26 +92,34 @@ const forecast = (() => {
             setWeather(
                 data.name,
                 data.sys.country,
+                data.weather[0].main,
+                data.weather[0].icon,
+                data.weather[0].description,
                 data.main.feels_like,
                 data.main.temp,
                 data.main.temp_max,
                 data.main.temp_min
             );
             getWeather();
-            generateMap(data.coord.lat, data.coord.lon);
+            interactiveMap.setMap(data.coord.lat, data.coord.lon);
             updateDOM();
         } catch (error) {
             //handle error
-            console.log(error);
+            alert(error);
+        }
+        loading.classList.add("hidden");
+    };
+    const updateUnit = (selection) => {
+        // do nothing if unit is the same
+        if (fahrenheit != selection) {
+            fahrenheit = selection;
+            console.log("updated to", selection);
+            updateWeather();
+            updateDOM();
         }
     };
-    const updateUnit = () => {
-        fahrenheit = !fahrenheit;
-        console.log("updated to", fahrenheit);
-        updateWeather();
-    };
     const updateWeather = () => {
-        if (fahrenheit) {
+        if (!fahrenheit) {
             weather.feels_like = ((weather.feels_like - 32) * (5 / 9)).toFixed(
                 1
             );
@@ -90,35 +127,63 @@ const forecast = (() => {
             weather.high = ((weather.high - 32) * (5 / 9)).toFixed(1);
             weather.low = ((weather.low - 32) * (5 / 9)).toFixed(1);
         } else {
-            weather.feels_like = (weather.feels_like * (5 / 9) + 32).toFixed(1);
-            weather.temp = (weather.temp * (5 / 9) + 32).toFixed(1);
-            weather.high = (weather.high * (5 / 9) + 32).toFixed(1);
-            weather.low = (weather.low * (5 / 9) + 32).toFixed(1);
+            weather.feels_like = (weather.feels_like * (9 / 5) + 32).toFixed(1);
+            weather.temp = (weather.temp * (9 / 5) + 32).toFixed(1);
+            weather.high = (weather.high * (9 / 5) + 32).toFixed(1);
+            weather.low = (weather.low * (9 / 5) + 32).toFixed(1);
         }
     };
     const updateDOM = () => {
+        const city = document.querySelector("#city");
+        const country = document.querySelector("#country");
+        const weather_icon = document.querySelector("#weather_icon");
         const feels_like = document.querySelector("#temp_feels_like");
         const temp = document.querySelector("#temp");
         const temp_high = document.querySelector("#temp_high");
         const temp_low = document.querySelector("#temp_low");
-        feels_like.textContent = weather.feels_like;
-        temp.textContent = weather.temp;
-        temp_high.textContent = weather.high;
-        temp_low.textContent = weather.low;
+        city.textContent = weather.city;
+        country.textContent = weather.country;
+        weather_icon.src = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+        feels_like.textContent = "Feels like: " + weather.feels_like;
+        temp.textContent = "Current temp: " + weather.temp;
+        temp_high.textContent = "High: " + weather.high;
+        temp_low.textContent = "Low: " + weather.low;
     };
     return { setWeather, getWeather, fetchWeather, updateUnit };
 })();
 
+function parseSearch(search) {
+    if (search.value.length < 1) {
+        alert("Please enter a city name!");
+    } else {
+        forecast.fetchWeather(search.value);
+    }
+}
+
 document.addEventListener(
     "DOMContentLoaded",
     function () {
-        const temp_btns = document.querySelectorAll(".temp");
-        for (let i = 0; i < temp_btns.length; i++) {
-            temp_btns[i].addEventListener("click", function () {
-                forecast.updateUnit();
-            });
-        }
-        forecast.fetchWeather();
+        const f_btn = document.querySelector("#fahrenheit");
+        f_btn.addEventListener("click", (e) => {
+            e.currentTarget.classList.add("active");
+            e.currentTarget.previousElementSibling.classList.remove("active");
+            forecast.updateUnit(true);
+        });
+        const c_btn = document.querySelector("#celcius");
+        c_btn.addEventListener("click", (e) => {
+            e.currentTarget.classList.add("active");
+            e.currentTarget.nextElementSibling.classList.remove("active");
+            forecast.updateUnit(false);
+        });
+        const search = document.querySelector("#search");
+        const search_btn = document.querySelector("#search-btn");
+        search_btn.addEventListener("click", () => parseSearch(search));
+        search.addEventListener("keypress", (e) => {
+            if (e.key == "Enter") {
+                parseSearch(search);
+            }
+        });
+        forecast.fetchWeather("San Francisco");
     },
     false
 );
